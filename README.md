b7da2f6c6e9d339a9f8e7cd305f0e5e509c92988
Pungushe <y.borozna81@internet.ru>
2021-10-26 20:44:02 +0300

    It's a restaurante site

---
+++ README.md
@@ -0,0 +1,7 @@
+Репозиторий сайта cook_blog на Django 3
+
+С добавлением Docker
+
+Установка (для пользователей операционных систем семейства MacOs/Linux/Windows):
+
+Открыть терминал или консоль и перейти в нужную Вам директорию Прописать команду git clone git@github.com:Pungushe/cook_blog.git Если Вы используете https, то: https://github.com/Pungushe/cook_blog.git
---
+++ cookblog/contact/models.py
@@ -0,0 +1,77 @@
+from ckeditor.fields import RichTextField
+from django.db import models
+
+
+class ContactModel(models.Model):
+    """ Класс модели обратной связи"""
+    name = models.CharField(max_length=50, verbose_name="Контакт")
+    email = models.EmailField()
+    website = models.URLField(blank=True, null=True)
+    message = models.TextField(max_length=5000)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+
+    class Meta:
+        verbose_name = "Обратная связь"
+        verbose_name_plural = "Обратная связь"
+
+
+class ContactLink(models.Model):
+    """ Класс модели контактов """
+    icon = models.FileField(upload_to="icons/")
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+
+
+class About(models.Model):
+    """ Класс модели страницы о нас """
+    name = models.CharField(max_length=50, default='')
+    text = RichTextField()
+    mini_text = RichTextField()
+
+    def __str__(self):
+        return self.name
+
+    def get_first_image(self):
+        item = self.about_images.first()
+        return item.image.url
+
+    def get_images(self):
+        return self.about_images.order_by('id')[1:]
+
+    class Meta:
+        verbose_name = "О нас"
+        verbose_name_plural = "О нас"
+
+
+class ImageAbout(models.Model):
+    """ Класс модели изображений страницы о нас """
+    image = models.ImageField(upload_to="about/")
+    page = models.ForeignKey(About, on_delete=models.CASCADE, related_name="about_images")
+    alt = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Изображения"
+        verbose_name_plural = "Изображений"
+
+
+class Social(models.Model):
+    """ Класс модели социалных сетей страницы о нас """
+    icon = models.FileField(upload_to="icons/")
+    name = models.CharField(max_length=200)
+    link = models.URLField()
+
+    class Meta:
+        verbose_name = "Социалная сеть"
+        verbose_name_plural = "Социалные сети"
+
+    def __str__(self):
+        return self.name    
